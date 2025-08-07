6c2ac2346335a75e21527e4513ced6aace78b9b6
armanmd85000 <154997050+armanmd85000@users.noreply.github.com>
2025-08-07 21:00:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,28 @@ import os
 
 class Config:
     # VJ-Forward-Bot core configs
-    API_ID = int(os.environ.get("API_ID", "25570420"))
+    API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_SESSION = os.environ.get("BOT_SESSION", "vjbot")
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "7552584508"))
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 # If you want to support loading direct gita1 variables/envs independently, you could do:
 GITA1_EXTRA = {
